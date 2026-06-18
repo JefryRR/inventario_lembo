b9f13addd19d1a260968345b06d3e5681ec966e1
Luisa-039 <luisasan8b@gmail.com>
2026-06-18 11:28:18 -0500

    Actualizando Dashboard

--- frontend/src/components/header/UserDropdown.tsx
+++ frontend/src/components/header/UserDropdown.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { Link } from "react-router";
+import {
+  UserCircleIcon as UserIcon,
+} from "../../icons";
 
 type LoggedUser = {
   nombre_user?: string;
@@ -37,7 +40,7 @@ export default function UserDropdown() {
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
         <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <img src="/images/user/owner.jpg" alt="User" />
+          <UserIcon className="h-10 w-10" />
         </span>
 
         <span className="block mr-1 font-medium text-theme-sm"> {user?.nombre_user || "Usuario"}</span>
